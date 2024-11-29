8e29a2ac3b30c75f362c28bc6a1fc6c66ec9819e
Finn Delaney <150743542+FinnDelaney00@users.noreply.github.com>
2024-11-29 11:01:17 +0000

    Update test_sodoku.py

--- test_sodoku.py
+++ test_sodoku.py
@@ -5,6 +5,24 @@ import unittest
 from sodoku import solve_sudoku
 
 class TestSudokuSolver(unittest.TestCase):
+
+    def is_valid_sudoku(self, board):
+        """Helper function to check if the Sudoku board is valid"""
+        for i in range(9):
+            # Check if all rows contain unique numbers from 1 to 9 (ignore 0)
+            if len(set(board[i])) != len([x for x in board[i] if x != 0]):
+                return False
+            # Check if all columns contain unique numbers from 1 to 9 (ignore 0)
+            if len(set(board[j][i] for j in range(9))) != len([board[j][i] for j in range(9) if board[j][i] != 0]):
+                return False
+            # Check 3x3 subgrids
+            row_start = (i // 3) * 3
+            col_start = (i % 3) * 3
+            subgrid = [board[row_start + r][col_start + c] for r in range(3) for c in range(3)]
+            if len(set(subgrid)) != len([x for x in subgrid if x != 0]):
+                return False
+        return True
+
     def test_valid_easy(self):
         board = [
             [5, 3, 0, 0, 7, 0, 0, 0, 0],
@@ -18,25 +36,39 @@ class TestSudokuSolver(unittest.TestCase):
             [0, 0, 0, 0, 8, 0, 0, 7, 9]
         ]
         solution = solve_sudoku(board)
-        self.assertEqual(solution, [
-            [5, 3, 4, 6, 7, 8, 9, 1, 2],
-            [6, 7, 2, 1, 9, 5, 3, 4, 8],
-            [1, 9, 8, 3, 4, 2, 5, 6, 7],
-            [8, 5, 9, 7, 6, 1, 4, 2, 3],
-            [4, 2, 6, 8, 5, 3, 7, 9, 1],
-            [7, 1, 3, 9, 2, 4, 8, 5, 6],
-            [9, 6, 1, 5, 3, 7, 2, 8, 4],
-            [2, 8, 7, 4, 1, 9, 6, 3, 5],
-            [3, 4, 5, 2, 8, 6, 1, 7, 9]
-        ])
+        self.assertIsNotNone(solution)
+        self.assertTrue(self.is_valid_sudoku(solution))
+
+    def test_valid_medium(self):
+        board = [
+            [8, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 3, 6, 0, 0, 0, 0, 0],
+            [0, 7, 0, 0, 9, 0, 2, 0, 0],
+            [0, 5, 0, 0, 0, 7, 0, 0, 0],
+            [0, 0, 0, 0, 4, 5, 7, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 3, 0],
+            [0, 0, 1, 0, 0, 0, 0, 6, 8],
+            [0, 0, 8, 5, 0, 0, 0, 1, 0],
+            [0, 9, 0, 0, 0, 0, 4, 0, 0]
+        ]
+        solution = solve_sudoku(board)
+        self.assertIsNotNone(solution)
+        self.assertTrue(self.is_valid_sudoku(solution))
 
     def test_unsolvable(self):
         board = [
             [5, 5, 0, 0, 7, 0, 0, 0, 0],
             [6, 0, 0, 1, 9, 5, 0, 0, 0],
-            # Incomplete board for illustration
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
         ]
-        self.assertIsNone(solve_sudoku(board))
+        solution = solve_sudoku(board)
+        self.assertIsNone(solution)
 
     def test_invalid_input(self):
         board = [
@@ -55,46 +87,28 @@ class TestSudokuSolver(unittest.TestCase):
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
         ]
-        self.assertIsNone(solve_sudoku(board))
+        solution = solve_sudoku(board)
+        self.assertIsNotNone(solution)
+        self.assertTrue(self.is_valid_sudoku(solution))
 
     def test_single_value_sudoku(self):
         board = [
-            [5, 3, 0, 0, 7, 0, 0, 0, 0],
-            [6, 0, 0, 1, 9, 5, 0, 0, 0],
-            [0, 9, 8, 0, 0, 0, 0, 6, 0],
-            [8, 0, 0, 0, 6, 0, 0, 0, 3],
-            [4, 0, 0, 8, 0, 3, 0, 0, 1],
-            [7, 0, 0, 0, 2, 0, 0, 0, 6],
-            [0, 6, 0, 0, 0, 0, 2, 8, 0],
-            [0, 0, 0, 4, 1, 9, 0, 0, 5],
-            [0, 0, 0, 0, 8, 0, 0, 7, 9]
-        ]
-        result = solve_sudoku(board)
-        self.assertIsNone(result)
-
-    def test_valid_medium(self):
-        board = [
-            [8, 1, 2, 0, 5, 3, 0, 4, 9],
-            [9, 4, 3, 0, 8, 2, 1, 7, 5],
-            [1, 5, 0, 0, 9, 0, 0, 8, 3],
-            [6, 7, 5, 4, 9, 1, 2, 8, 3],
-            [2, 3, 1, 7, 5, 0, 0, 9, 6],
-            [4, 9, 0, 3, 1, 6, 7, 5, 2],
-            [7, 0, 0, 9, 4, 8, 6, 3, 1],
-            [5, 2, 9, 1, 3, 7, 8, 6, 4],
-            [3, 6, 8, 2, 7, 5, 9, 1, 0]
+            [5, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
         ]
         solution = solve_sudoku(board)
-        self.assertEqual(solution, [
-            [8, 1, 2, 6, 5, 3, 7, 4, 9],
-            [9, 4, 3, 8, 2, 7, 1, 5, 6],
-            [1, 5, 7, 4, 9, 6, 2, 8, 3],
-            [6, 7, 5, 4, 9, 1, 2, 8, 3],
-            [2, 3, 1, 7, 5, 9, 4, 9, 6],
-            [4, 9, 2, 8, 3, 6, 7, 5, 2],
-        ])
+        self.assertIsNotNone(solution)
+        self.assertTrue(self.is_valid_sudoku(solution))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
